ca64a89e32dc69871e8b8ce4bda464aa2437a7cc
cristianbonomo <cristian.a.bonomo@gmail.com>
2020-05-10 16:39:08 -0300

    boton generar funcionando

--- src/index.js
+++ src/index.js
@@ -1,35 +1,26 @@
 //guardamos la API URL
 const API = 'https://rickandmortyapi.com/api/character/'
+const btnLoad = document.getElementsByClassName('.btnCharacter');
 //selectores HTML: seleccionamos y guardamos la posicion donde insertaremos el codigo.
 const $characterContainer = document.querySelector('.character--container');
 //generamos la funcion para traer la data 
-(async function load() {
     async function getData(url) {
         const response = await fetch(url);
         const data = await response.json();
-        return data;
+        htmlString = ` <div class="character--container">
+        <div class="character--image-container">
+        <img class="character_img" src="https://rickandmortyapi.com/api/character/avatar/${data.id}.jpeg" alt="Photo">
+        </div>
+        <h3 class="character_name">Nombre: ${data.name}</h3>
+        <h4 class="character_species">Especie: ${data.species}</h4>
+        <h4 class="character_origin">Origen: ${data.origin.name}</h4>`;
+    $characterContainer.innerHTML = htmlString;
     }
+
+function getCharacter(){
 //generamos un id random para la URL
 let id = Math.round(Math.random() * (493 - 1) + 1);
-const characterList = await getData(`${API}${id}/`);
-//generamos el template de divs a insertar en nuestro HTML
-function characterTemplate(characterList) {
-    htmlString = ` <div class="character--container">
-    <div class="character--image-container">
-    <img class="character_img" src="https://rickandmortyapi.com/api/character/avatar/${id}.jpeg" alt="Photo">
-    </div>
-    <h3 class="character_name">Nombre: ${characterList.name}</h3>
-    <h4 class="character_species">Especie: ${characterList.species}</h4>
-    <h4 class="character_origin">Origen: ${characterList.origin.name}</h4>`;
-    return htmlString;
+const characterList = getData(`${API}${id}`)
 }
-//guardamos en una variable los datos a sumar al HTML
-htmlString = characterTemplate(characterList);
-//Y los insertamos.
-$characterContainer.innerHTML+=htmlString;
-})();//llamamos a la funcion para inicializar
-
-
-
 
 
